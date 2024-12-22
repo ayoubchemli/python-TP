96c8cfe677109f9a8dc2fcaaacdd482271f26758
ayouub <chemliayoub414@gmail.com>
2024-12-22 20:44:55 +0100

    exo 13

---
+++ Exercice13.py
@@ -0,0 +1,11 @@
+while True:
+    print(" \n")
+    user_input = input("Please type in a string: ") 
+    if user_input == "":  
+        break  
+    
+    print(" \n")
+    print(user_input)
+    print("-" * len(user_input))  
+    
+
